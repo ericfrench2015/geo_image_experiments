671b12ffe4420ec13870d29c4619a1096a522596
Eric French <ericfrench2015@gmail.com>
2024-09-29 08:09:16 -0500

    adding cleaned up files so the pipeline code can hopefully be run successfully.

--- src/image_and_detection_selector.py
+++ src/image_and_detection_selector.py
@@ -20,6 +20,7 @@ def select_detections(df_detections, selected_detection):
         for detection in unqiue_detections:
             if re.match(selected_detection, detection):
                 matched_detections.append(detection)
+        matched_detections.sort()
         return matched_detections
 
     unique_detections = list(set(df_detections.detection_label.tolist()))
@@ -42,16 +43,12 @@ def find_images_matching(df_detections, selected_detections, threshold=5, match_
 
     """
 
-    print(selected_detections)
-
-    # first, drop all detections under the threshold
-    print(df_detections.shape)
-    # temp_df = df_detections[df_detections.detection_prct_of_image >= threshold]
-    # print(temp_df.shape)
+    print(f"original detections count is {len(df_detections)}")
 
     # first, reduce df to only selected detections
     temp_df = df_detections[df_detections['detection_label'].isin(selected_detections)]
-    print(temp_df.shape)
+    print(f"after filtering for desired detections: {len(temp_df)}")
+    #print(temp_df.shape)
 
     # for each image, get the detection label count matched and the total percent of image taken
     temp_images = temp_df[['image_id', 'detection_label', 'detection_prct_of_image']].groupby('image_id').agg(
